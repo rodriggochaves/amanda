75216be8117f6b242bb9b964a16d8d1bce9ce2f9
rodriggochaves <rodriggo.chaves@gmail.com>
2017-11-03 14:36:04 -0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'rubocop'
+gem 'dot_env', '~> 0.0.3'
+gem 'octokit', '~> 4.7'
+gem 'git_diff_parser'
---
+++ amanda.rb
@@ -0,0 +1,43 @@
+require 'rubocop'
+require 'dot_env'
+require 'octokit'
+require 'pp'
+require 'git_diff_parser'
+
+DotEnv.get_environment
+
+class Amanda
+  def initialize
+    Octokit.configure do |c|
+      c.login = ENV['GITHUB_LOGIN']
+      c.password = ENV['GITHUB_PASSWORD']
+    end
+
+    name = 'rodriggochaves/literate-lamp'
+
+    cop = RuboCop::Cop::Style::StringLiterals
+    team = RuboCop::Cop::Team.new(RuboCop::Cop::Registry.new([cop]), 
+                                  RuboCop::ConfigLoader.default_configuration,
+                                  { auto_correct: true })
+
+    pull_requests = Octokit.pull_requests(name, status: 'open')[0]
+
+    head_sha = pull_requests[:head][:sha]
+
+    files = Octokit.pull_request_files(name, 3)
+
+    example = files[0]
+    # commit = Octokit.commit(name, head_sha)
+
+    patch = GitDiffParser::Patch.new(example[:patch])
+
+    patch.changed_lines.each do |l|
+      pp team.auto_correct(l.content, [cop])
+    end
+
+    
+
+    # usar o octokit para recuperar os arquivos
+    # usar o metodo .inspect_file
+  end
+end
