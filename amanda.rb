2c1191e92ea17d3c0c565f08a57cd41db226f6de
Renan Lobato Rheinboldt <renan.lobato.rheinboldt@gmail.com>
2017-11-06 01:18:35 -0200

    clone repo with rugged

--- amanda.rb
+++ amanda.rb
@@ -3,39 +3,47 @@ require 'dot_env'
 require 'octokit'
 require 'pp'
 require 'git_diff_parser'
+require 'rugged'
 
 DotEnv.get_environment
 
 class Amanda
   def initialize
-    Octokit.configure do |c|
-      c.login = ENV['GITHUB_LOGIN']
-      c.password = ENV['GITHUB_PASSWORD']
-    end
+    Rugged::Repository.clone_at("https://github.com/rodriggochaves/literate-lamp.git", "./tmp",
+                                {transfer_progress: lambda { |total_objects, indexed_objects,
+                                    received_objects, local_objects, total_deltas, indexed_deltas, received_bytes|
+                                  pp received_bytes, total_objects, received_objects}})
 
-    name = 'rodriggochaves/literate-lamp'
+    # Octokit.configure do |c|
+    #   c.login = ENV['GITHUB_LOGIN']
+    #   c.password = ENV['GITHUB_PASSWORD']
+    # end
+    #
+    # name = 'rodriggochaves/literate-lamp'
+    #
+    #
+    # pull_requests = Octokit.pull_requests(name, status: 'open')[0]
+    #
+    #
+    # head_sha = pull_requests[:head][:sha]
+    #
+    # files = Octokit.pull_request_files(name, 3)
+    #
+    # pp files
+    # example = files[0]
+    # # commit = Octokit.commit(name, head_sha)
+    #
+    # patch = GitDiffParser::Patch.new(example[:patch])
+    #
+    # cop = RuboCop::Cop::Style::StringLiterals
+    # team = RuboCop::Cop::Team.new(RuboCop::Cop::Registry.new([cop]),
+    #                               RuboCop::ConfigLoader.default_configuration,
+    #                               {auto_correct: true})
+    # patch.changed_lines.each do |l|
+    #   pp team.auto_correct(l.content, [cop])
+    # end
 
 
-    pull_requests = Octokit.pull_requests(name, status: 'open')[0]
-
-
-    head_sha = pull_requests[:head][:sha]
-
-    files = Octokit.pull_request_files(name, 3)
-
-    pp files
-    example = files[0]
-    # commit = Octokit.commit(name, head_sha)
-
-    patch = GitDiffParser::Patch.new(example[:patch])
-
-    cop = RuboCop::Cop::Style::StringLiterals
-    team = RuboCop::Cop::Team.new(RuboCop::Cop::Registry.new([cop]),
-                                  RuboCop::ConfigLoader.default_configuration,
-                                  {auto_correct: true})
-    patch.changed_lines.each do |l|
-      pp team.auto_correct(l.content, [cop])
-    end
 
     
 
