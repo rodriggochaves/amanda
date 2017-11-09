4c66e98013c53c869b804eb95dcbb2a132104ddb
Renan Lobato Rheinboldt <renan.lobato.rheinboldt@gmail.com>
2017-11-09 14:58:19 -0200

    add todos

--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
-require_relative './amanda'
+require_relative './billy_bot'
 
-Amanda.new("rodriggochaves/literate-lamp","-R")
+# TODOS
+# 1) Rodar analisador apenas sobre arquivos modificados no commit
+# 2) Criar branch novo com alterações
+# 3) Criar pull-request e realizar push
+# 4) Criar comentários amigáveis no pull-request
+# 5) Criar uma gema com o projeto
+# 6) Criar api para automatizar o processo de correção
+# 7) Expandir para outras linguagens
+
+BillyBot.new("rodriggochaves/literate-lamp", "-R")
