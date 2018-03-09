ff8a3970047a54f849b31cd7a214b748689b651d
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-03-09 16:07:18 -0300

    method to parse output of rubocop

--- lib/amanda_bot/analyzers/ruby_analyzer.rb
+++ lib/amanda_bot/analyzers/ruby_analyzer.rb
@@ -11,13 +11,24 @@ class RubyAnalyzer < Analyzer
   end
 
   def analyze files
-    @files = files
+    @files = files.select{ |f| extensions.include?(File.extname(f)) }
     run_rubocop
+  end
+
+  def parse result
+    result.split("\n").map do |line|
+      if line.include?("[Corrected]")
+        line[(line.index("[Corrected]") + 11)..(line.length)]
+      else
+        ""
+      end
+    end
   end
 
   private def run_rubocop
     @files.map{ |e| "#{TMP_FOLDER}/#{@repo}/#{e}" }.each do |file|
-      system "rubocop -a #{file}"
+      result = `rubocop -a #{file}`
+      @report = parse(result).reject{ |s| s.empty? }
     end
   end
 end
